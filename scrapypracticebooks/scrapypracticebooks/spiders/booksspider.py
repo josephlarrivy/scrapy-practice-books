df163f8318a3e666c6c60595c1ef8cf76af64a24
josephlarrivy <joseph.larrivy@gmail.com>
2023-04-18 13:21:46 -0500

    first commit

---
+++ scrapypracticebooks/scrapypracticebooks/spiders/booksspider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+class BooksSpider(scrapy.Spider):
+    name = 'booksspider'
+    start_urls = ['http://books.toscrape.com/']
+
+    def parse(self, response):
+        items = response.css('div.col-sm-8.col-md-9')
+
+        header = items.css('div.page-header.action')
+        header.css('h1::text').get()
+        
+        main_items_section = items.css('section')
+        items = main_items_section.css('ol.row')
+        
+        for item in items:
+            container = items.css('li.col-xs-6.col-sm-4.col-md-3.col-lg-3')
+            inner_container = container.css('article.product_pod')
+
+            price_container = inner_container.css('div.product_price')
+            price = price_container.css('p.price_color::text').get()
+
+            yield {
+                'price' : price
+            }
